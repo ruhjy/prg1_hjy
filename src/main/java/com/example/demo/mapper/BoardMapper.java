b3e003a763faa1ed865ab0d5284a65e549139774
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-05-02 16:58:58 +0900

    실패작

--- src/main/java/com/example/demo/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -3,31 +3,31 @@ package com.example.demo.mapper;
 import java.util.*;
 
 import org.apache.ibatis.annotations.*;
-import org.springframework.stereotype.*;
 
 import com.example.demo.domain.*;
 
-@Mapper
+//@Mapper
 public interface BoardMapper {
 
-	@Select("select id, title, writer, inserted from Board order by id desc")
+//	@Select("select id, title, writer, inserted from Board order by id desc")
 	List<Board> selectAll();
 
-	@Select("select * from Board where id = #{id}")
+//	@Select("select * from Board where id = #{id}")
+//	@ResultMap("boardResultMap")
 	Board selectById(Integer id);
 
-	@Update("update Board "
-			+ "set title = #{title}, "
-			+ "	   body = #{body}, "
-			+ "    writer = #{writer} "
-			+ "where id = #{id}")
+//	@Update("update Board "
+//			+ "set title = #{title}, "
+//			+ "	   body = #{body}, "
+//			+ "    writer = #{writer} "
+//			+ "where id = #{id}")
 	int update(Board board);
 
-	@Delete("delete from Board where id = #{id}")
+//	@Delete("delete from Board where id = #{id}")
 	int deleteById(Integer id);
 
-	@Insert("insert into Board (title, body, writer) values (#{title}, #{body}, #{writer})")
-	@Options(useGeneratedKeys = true, keyProperty = "id")
+//	@Insert("insert into Board (title, body, writer) values (#{title}, #{body}, #{writer})")
+//	@Options(useGeneratedKeys = true, keyProperty = "id")
 	int insert(Board board);
 
 //	@Select("<script> "
@@ -40,21 +40,21 @@ public interface BoardMapper {
 //			+ "</script>")
 //	List<Board> selectAllPaging(Integer startIndex, Integer rowPerPage, String search);
 
-	@Select("<script> "
-			+ "    select id, title, writer, inserted from Board "
-			+ "<where> "
-			+ "		<if test=\"(type eq \'all\') or (type eq \'title\')\"> "
-			+ "			or title like concat(\'%\', #{search}, \'%\') "
-			+ "		</if> "
-			+ "		<if test=\"(type eq \'all\') or (type eq \'body\')\"> "
-			+ "			or body like concat(\'%\', #{search} ,\'%\') "
-			+ "		</if> "
-			+ "		<if test=\"(type eq \'all\') or (type eq \'writer\')\"> "
-			+ "			or writer like concat(\'%\', #{search} ,\'%\') "
-			+ "		</if> "
-			+ "</where>"
-			+ "order by id desc limit #{startIndex}, #{rowPerPage} "
-			+ "</script>")
+//	@Select("<script> "
+//			+ "    select id, title, writer, inserted from Board "
+//			+ "<where> "
+//			+ "		<if test=\"(type eq \'all\') or (type eq \'title\')\"> "
+//			+ "			or title like concat(\'%\', #{search}, \'%\') "
+//			+ "		</if> "
+//			+ "		<if test=\"(type eq \'all\') or (type eq \'body\')\"> "
+//			+ "			or body like concat(\'%\', #{search} ,\'%\') "
+//			+ "		</if> "
+//			+ "		<if test=\"(type eq \'all\') or (type eq \'writer\')\"> "
+//			+ "			or writer like concat(\'%\', #{search} ,\'%\') "
+//			+ "		</if> "
+//			+ "</where>"
+//			+ "order by id desc limit #{startIndex}, #{rowPerPage} "
+//			+ "</script>")
 	List<Board> selectAllPaging(Integer startIndex, Integer rowPerPage, String search, String type);
 
 //	@Select("<script> "
@@ -75,21 +75,23 @@ public interface BoardMapper {
 //			+ "order by id desc limit #{startIndex}, #{rowPerPage}")
 //	List<Board> selectAllPaging(Integer startIndex, Integer rowPerPage, String search);
 
-	@Select("<script> "
-			+ "<bind name=\"pattern\" value=\"\'%\' + search + \'%\'\" /> "
-			+ "		select count(*) from Board "
-			+ "<where> "
-			+ "		<if test=\"(type eq \'all\') or (type eq \'title\')\"> "
-			+ "			or title like #{pattern} "
-			+ "		</if> "
-			+ "		<if test=\"(type eq \'all\') or (type eq \'body\')\"> "
-			+ "			or body like #{pattern} "
-			+ "		</if> "
-			+ "		<if test=\"(type eq \'all\') or (type eq \'writer\')\"> "
-			+ "			or writer like #{pattern} "
-			+ "		</if> "
-			+ "</where>"
-			+ "</script>")
+//	@Select("<script> "
+//			+ "<bind name=\"pattern\" value=\"\'%\' + search + \'%\'\" /> "
+//			+ "		select count(*) from Board "
+//			+ "<where> "
+//			+ "		<if test=\"(type eq \'all\') or (type eq \'title\')\"> "
+//			+ "			or title like #{pattern} "
+//			+ "		</if> "
+//			+ "		<if test=\"(type eq \'all\') or (type eq \'body\')\"> "
+//			+ "			or body like #{pattern} "
+//			+ "		</if> "
+//			+ "		<if test=\"(type eq \'all\') or (type eq \'writer\')\"> "
+//			+ "			or writer like #{pattern} "
+//			+ "		</if> "
+//			+ "</where>"
+//			+ "</script>")
 	Integer countAll(String search, String type);
+
+	Integer insertFileName(Integer boardId, String fileName);
 
 }
