aa9abdca4e9fc3aca7b436186b59dd5ac5e0e896
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-05-09 15:20:56 +0900

    Create 1234.sql

---
+++ src/main/webapp/sql/project/1234.sql
@@ -0,0 +1,22 @@
+use Board;
+
+select * from Board order by id desc;
+select * from FileName;
+select * from Board order by id desc;
+
+select * from Board b
+	join FileName f on b.id = f.boardId;
+    
+desc Board;
+desc FileName;
+desc Member;
+
+update Board set hit = hit + 1 where id = 2;
+alter table Board add column hit int after writer;
+
+select * from Member order by inserted desc;
+select id, password from Member where id = 'test2';
+
+
+
+
