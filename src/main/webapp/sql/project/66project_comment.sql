1380177ccdd9e21f70e926a3a45291d074594dcb
hjy <ruhjy@naver.com>
2023-05-22 09:59:09 +0900

    댓글 테이블

---
+++ src/main/webapp/sql/project/66project_comment.sql
@@ -0,0 +1,22 @@
+use Board;
+
+desc Comment;
+select * from Comment;
+create table Comment(
+	id int primary key auto_increment,
+	boardId int not null,
+    memberId varchar(20) not null,
+    content varchar(500) not null,
+    inserted datetime not null default now(),
+    foreign key (boardId) references Board(id),
+    foreign key (memberId) references Member(id)
+);
+
+select * from Member;
+
+select count(*) from Comment where boardId = 2144;
+
+insert into Comment (boardId, memberId, content) values (2144, 'god', 'ㅎ2');
+insert into Comment (boardId, memberId, content) values (2144, 'god', 'ㅎ22');
+insert into Comment (boardId, memberId, content) values (2144, 'god', 'ㅎ22');
+insert into Comment (boardId, memberId, content) values (2144, 'while', 'dd');
